35f08212879e6a7df28beb7f350f9d8b7be3f253
thembahinke <themba.hinke@gmail.com>
2016-12-04 07:56:07 -0500

    Initial Commit

---
+++ learningToCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learningToCode
+//
+//  Created by Themba Hinke on 12/4/16.
+//  Copyright © 2016 Themba Hinke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
